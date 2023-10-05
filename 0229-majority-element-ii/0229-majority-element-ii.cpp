da14c54e19b4f9060ce26bf399b10e3b324ed85a
Lazy_lozer <52577276+lucky-2104@users.noreply.github.com>
2023-10-05 22:00:31 +0530

    Time: 9 ms (61.98%), Space: 16.3 MB (9.38%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -6,19 +6,12 @@ public:
 
         vector<int> ans;
         for(int i = 0 ; i < nums.size() ; i++)
-        {
-            if(count.find(nums[i]) == count.end())
-            count.insert({nums[i] ,1});
-            else
-            count[nums[i]]++;
-        }
-
+        count[nums[i]]++;
         for(auto it = count.begin() ; it != count.end();it++)
         {
             if(it->second > nums.size()/3)
             ans.push_back(it->first);
         }
         return ans;
-        
     }
 };
