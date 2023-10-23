9796da1beb373f6a7f562a45fc3fda5f0d39003c
Lazy_lozer <52577276+lucky-2104@users.noreply.github.com>
2023-10-23 09:50:02 +0530

    Time: 4 ms (22.26%), Space: 6.3 MB (26.74%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+class Solution {
+public:
+
+    bool isPowerOfFour(int n) {
+        int mask = 0x55555555;
+        return n > 0 && (n & (n - 1)) == 0 && (n & mask) == n;
+
+    }
+};
