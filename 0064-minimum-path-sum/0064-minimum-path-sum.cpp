08e103ca89c04765d4e2222959f67fed3883c561
Lazy_lozer <52577276+lucky-2104@users.noreply.github.com>
2023-10-25 05:54:01 +0530

    Time: 3 ms (97.97%), Space: 10.4 MB (37%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -20,7 +20,26 @@ public:
 
     }
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>> dp(grid.size() , vector<int>(grid[0].size(),-1));
-        return sum_min(grid , grid.size()-1 , grid[0].size()-1 , dp);
+        vector<vector<int>> dp(grid.size() , vector<int>(grid[0].size(),0));
+
+        dp[0][0] = grid[0][0];
+
+        for(int i = 0 ; i < grid.size() ; i++)
+        {
+            for(int j = 0 ; j < grid[0].size() ; j++ )
+            {
+                if(i == 0 and j ==0 )
+                continue;
+                else
+                {
+                    int down = i > 0 ? grid[i][j] + dp[i-1][j] : 1e9;
+                    int right = j > 0 ?grid[i][j] + dp[i][j-1] : 1e9;
+                    dp[i][j] = min(down,right);
+                }
+            }
+        }
+
+        return dp[grid.size()-1][grid[0].size()-1];
+        // return sum_min(grid , grid.size()-1 , grid[0].size()-1 , dp);
     }
 };
