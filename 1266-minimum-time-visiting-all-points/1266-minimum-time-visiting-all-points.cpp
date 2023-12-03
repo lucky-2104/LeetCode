4eef7f5b67970fc351c7b68614619ae89138ea73
Lazy_lozer <52577276+lucky-2104@users.noreply.github.com>
2023-12-03 21:17:14 +0530

    Time: 11 ms (13.24%), Space: 10.6 MB (14.02%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -9,12 +9,10 @@ public:
         
         for(int i = 1 ; i < points.size() ; i++)
         {
-            int x2 = points[i][0];
-            int y2 = points[i][1];
-            distance = distance + max(abs(x2-x1) , abs(y2-y1));
+            distance = distance + max(abs(points[i][0]-x1) , abs(points[i][1]-y1));
             
-            x1 = x2;
-            y1 = y2; 
+            x1 = points[i][0];
+            y1 = points[i][1]; 
         }
         
         return distance;
