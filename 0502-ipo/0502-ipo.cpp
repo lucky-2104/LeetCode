3cfc69d3dc0f2be6dcb147fbc8a994b56f26d048
Lazy_lozer <52577276+lucky-2104@users.noreply.github.com>
2024-06-15 20:44:36 +0530

    Time: 283 ms (6.24%), Space: 87.9 MB (17.24%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+
+        //Sort the array pick all the capitals smaller than w and insert them into max_priority_queue
+        // and select the k element from that priority queue; and if selected increase the w by profit[i] and repeat
+        //as soon as u increase the value of w
+
+        vector<pair<int,int>> vec;
+        for(int i = 0 ; i < profits.size() ; i++)   vec.push_back({capital[i] , profits[i]});
+
+        sort(vec.begin() , vec.end());
+        int count = 0;
+        priority_queue<pair<int,int>> pq;
+        for(int i = 0 ; i < vec.size() ; )
+        {
+            int cap = vec[i].first;
+            int pro = vec[i].second;
+            cout << cap << " " << pro << endl;
+            if(cap <= w)
+            {
+                pq.push({pro ,cap});
+                i++;
+            }
+            else if(k > 0 and !pq.empty()){
+                cout << "here " << endl;
+                int curr_cap = pq.top().second;
+                int curr_pro = pq.top().first;
+                pq.pop();
+                w = w + curr_pro;
+                k--;
+            }
+            else
+            break;
+        }
+
+        while(k > 0 and !pq.empty())
+        {
+            int curr_cap = pq.top().second;
+            int curr_pro = pq.top().first;
+            pq.pop();
+            w = w + curr_pro;
+            k--;
+        }
+
+        return w;
+        
+    }
+};
