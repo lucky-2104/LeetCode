d9265c2974862803c867182180ffa1b2655d6651
Lazy_lozer <52577276+lucky-2104@users.noreply.github.com>
2024-07-04 13:39:58 +0530

    Time: 442 ms (58.33%), Space: 278.1 MB (36.49%) - LeetHub

---
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+
+        ListNode * walk = head;
+
+        ListNode* new_head = NULL;
+        ListNode* tail = NULL;
+        while(walk->next != NULL)
+        {
+
+            if(walk->val == 0){
+                ListNode * new_node = new ListNode(0);
+                if(new_head == NULL and tail == NULL)
+                {
+                    new_head = new_node;
+                    tail = new_head;
+                }
+                else{
+                    tail->next = new_node;
+                    tail = tail->next;
+                }
+            }
+            else
+            tail->val = tail->val+walk->val;
+
+            walk = walk->next;
+        }
+        // delete tail;
+        return new_head;
+        
+    }
+};
