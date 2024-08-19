9dc7522575eba0c9de19e2f509239d5e91d5cf96
Lazy_lozer <52577276+lucky-2104@users.noreply.github.com>
2024-08-19 11:52:07 +0530

    Time: 4 ms (32.02%), Space: 10.1 MB (44.22%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,29 +11,32 @@
  */
 class Solution {
 public:
-    
-    void inorder(TreeNode* root , vector<int>& ans)
-    {
-        if(root== NULL)
-            return ;
-        
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
-        
-        
-        
-    }
+
     
     vector<int> inorderTraversal(TreeNode* root) {
-        
+
         vector<int> ans;
-        if(root==NULL)
-            return {};
-        inorder(root,ans);
-        
+        TreeNode* node = root;
+        stack<TreeNode*> st;
+
+        while(true)
+        {
+            if(node != NULL){
+                st.push(node);
+                node = node->left;
+            }
+            else
+            {
+                if(st.empty() == true) break;
+                node = st.top();
+                st.pop();
+                ans.push_back(node->val);
+
+                node = node->right;
+            }
+        } 
         return ans;
-        
+
         
     }
 };
