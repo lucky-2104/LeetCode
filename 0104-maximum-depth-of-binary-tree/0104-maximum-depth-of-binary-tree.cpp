98bfbbc1cc25c2d5164d798f3b9b69c2d8e19a54
Lazy_lozer <52577276+lucky-2104@users.noreply.github.com>
2024-08-19 12:02:03 +0530

    Time: 3 ms (92.4%), Space: 17.8 MB (29.95%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -22,26 +22,26 @@ public:
 
         return f(root);
 
-        if(root == NULL) return 0;
+        // if(root == NULL) return 0;
 
-        queue<TreeNode*> q;
-        q.push(root);
+        // queue<TreeNode*> q;
+        // q.push(root);
 
-        int level = 0;
-        while(!q.empty())
-        {
-            int len = q.size();
+        // int level = 0;
+        // while(!q.empty())
+        // {
+        //     int len = q.size();
 
-            for(int i = 0 ; i < len ; i++){
+        //     for(int i = 0 ; i < len ; i++){
 
-                TreeNode* node = q.front();
-                q.pop();
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-            }
-            level++;
-        }
-        return level;
+        //         TreeNode* node = q.front();
+        //         q.pop();
+        //         if(node->left) q.push(node->left);
+        //         if(node->right) q.push(node->right);
+        //     }
+        //     level++;
+        // }
+        // return level;
         
     }
 };
